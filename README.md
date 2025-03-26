e67dd9b9e6b344797beb8dc695eb6a43028ed9b2
rainer.wuttke@leoo.com.br <rainerzabel1@gmail.com>
2025-03-26 11:53:17 -0300

    add locale field

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # custom_activity_model_node
+
+
+## Fluxo de teste
+Comando LocalTunnel: "lt -p 8080 -s testerainer"
+Comando Servidor (Pasta backend > src): "node server.js"
--- backend/src/public/js/customActivity.js
+++ backend/src/public/js/customActivity.js
@@ -7,44 +7,16 @@ connection.trigger("ready");
 connection.trigger("requestSchema");
 connection.trigger("requestTokens");
 connection.trigger("requestTriggerEventDefinition");
-connection.trigger("updateButton", {
-    button: "next",
-    text: "done",
-    enabled: false,
-});
+connection.trigger("updateButton", { button: "next", text: "done", enabled: false });
 
 const schema = {};
+let dataExtensionFields = [];
 let body = { metadata: {} };
 
-connection.on(
-    "requestedTriggerEventDefinition",
-    function (eventDefinitionModel) {
-        if (eventDefinitionModel) {
-        eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
-        console.log("[EventDefinitionKey] " + eventDefinitionKey);
-        }
-    }
-);
-connection.on("requestedSchema", function (response) {
-    for (let item of response.schema) {
-        schema[item.name] = "{{" + item.key + "}}";
-    }
-});
-connection.on("initActivity", (data) => {
-    createdData = { ...createdData, ...data };
-    createdData.isConfigured = false;
-    createdData.metadata = createdData.metadata || {};
-    createdData.metadata.optStatus = null;
 
-    body = {
-        ...body,
-        ...(createdData.arguments || { execute: { body: "{}" } }).execute.body,
-    };
-    console.log("[body] " + body);
-});
 
+/* Configuração do front sobre erros na Atividade */
 const inputOptin = document.querySelector("#select-optin");
-const inputOptinErrorText = document.querySelector("#select-optin-error");
 inputOptin.addEventListener("focusout", function (event) {
     SetErrorMessage(event);
 });
@@ -52,36 +24,71 @@ inputOptin.addEventListener("change", function (event) {
     SetErrorMessage(event);
 });
 
-function SetErrorMessage(event) {
-    if (!event.target.value) {
-        inputOptin.classList.add("slds-has-error");
-        inputOptinErrorText.style.display = "block";
-        connection.trigger('updateButton', { button: 'next', text: 'done', enabled: true });
-    } else {
-        inputOptin.classList.remove("slds-has-error");
-        inputOptinErrorText.style.display = "none";
-        connection.trigger('updateButton', { button: 'next', text: 'done', enabled: false });
-    }
-     
-    createdData.metadata.optStatus = inputOptin.value;
-}
-
-connection.on("requestedTokens", onGetTokens);
-    function onGetTokens(tokens) {
-    authTokens = tokens;
-}
-
-connection.on("clickedNext", () => {
-    save();
+const inputLocale = document.querySelector("#select-localeField");
+inputLocale.addEventListener("focusout", function (event) {
+    SetErrorMessage(event);
+});
+inputLocale.addEventListener("change", function (event) {
+    SetErrorMessage(event);
 });
 
-const save = () => {
+function SetErrorMessage(event) {
+    if (!event.target.value) {
+        event.target.classList.add("slds-has-error");
+        connection.trigger('updateButton', { button: 'next', text: 'done', enabled: false }); 
+    } else {
+        event.target.classList.remove("slds-has-error");
+        connection.trigger('updateButton', { button: 'next', text: 'done', enabled: true });
+    }
+}
+
+
+
+/* Configurar campo de seleção */
+let firstOption = inputLocale.querySelector('option');
+for (let i = 0; i < dataExtensionFields.length; i++) {
+    let newOption = 
+}
+
+
+/* Execução dos eventos */
+connection.on( "requestedTriggerEventDefinition", (eventDefinitionModel) => { // ? Executado quando a ação "requestTriggerEventDefinition" ocorrer. Atualmente ao abrir a atividade.
+    if (eventDefinitionModel) {
+        eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
+        console.log("[EventDefinitionKey] " + eventDefinitionKey);
+    }
+});
+connection.on("requestedSchema", (response) => { // ? Executado quando a ação "requestSchema" ocorrer. Atualmente ao abrir a atividade.
+    console.log(JSON.stringify(response));
+    for (let item of response.schema) {
+        schema[item.name] = "{{" + item.key + "}}";
+        dataExtensionFields.push({ name: item.name, type: item.type });
+    }
+});
+connection.on("initActivity", (data) => { // ? Executado quando a atividade é aberta no Journey Builder
+    createdData = { ...createdData, ...data };
+    createdData.isConfigured = false;
+    createdData.metaData.optStatus = null;
+
+    body = {
+        ...body,
+        ...(createdData.arguments || { execute: { body: "{}" } }).execute.body,
+    };
+    console.log("[body] " + body);
+});
+connection.on("requestedTokens", (tokens) => { // ? Executado quando a ação "requestTokens" ocorrer. Atualmente ao abrir a atividade.
+    authTokens = tokens;
+});
+connection.on("clickedNext", () => { // ? Executado quando o botão de Next/Done for clicado
     createdData["metaData"].isConfigured = true;
     createdData["arguments"].execute.inArguments.push(schema);
     createdData.arguments.execute.body = JSON.stringify(body);
 
+    createdData.metaData.optStatus = inputOptin.value;
+    createdData.metaData.localeField = inputLocale.value;
+
     if (schema.undefined) delete schema.undefined;
 
-    console.log("[createdData] " + createdData);
+    console.log("[createdData] ", JSON.stringify(createdData));
     connection.trigger("updateActivity", createdData);
-};
+});
