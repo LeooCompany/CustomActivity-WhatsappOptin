cd26b7f454db6d5c743d20133d12d92803cc4987
rainer.wuttke@leoo.com.br <rainerzabel1@gmail.com>
2025-03-31 17:58:42 -0300

    Schema & InArguments Completo

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# custom_activity_model_node
+# Custom Activity - Whatsapp Optin
 
+## Links importantes
+https://developer.salesforce.com/docs/marketing/marketing-cloud/guide/custom-activity-config.html
+https://developer.salesforce.com/docs/marketing/marketing-cloud/guide/example-rest-activity.html
+https://developer.salesforce.com/docs/marketing/marketing-cloud/guide/using-postmonger.html#events-broadcast-by-journey-builder
 
 ## Fluxo de teste
 Comando LocalTunnel: "lt -p 8080 -s testerainer"
--- backend/src/public/js/customActivity.js
+++ backend/src/public/js/customActivity.js
@@ -7,124 +7,153 @@ connection.trigger("ready");
 connection.trigger("requestSchema");
 connection.trigger("requestTokens");
 connection.trigger("requestTriggerEventDefinition");
-connection.trigger("updateButton", { button: "next", text: "done", enabled: false });
+connection.trigger("updateButton", {
+  button: "next",
+  text: "done",
+  enabled: false,
+});
 
 const schema = {};
 let dataExtensionFields = [];
 let body = { metadata: {} };
 
-
-
 /* Configuração do front sobre erros na Atividade */
 const inputOptin = document.querySelector("#select-optin");
 inputOptin.addEventListener("focusout", function (event) {
-    SetErrorMessage(event);
+  SetErrorMessage(event);
 });
 inputOptin.addEventListener("change", function (event) {
-    SetErrorMessage(event);
+  SetErrorMessage(event);
 });
 
 const inputLocale = document.querySelector("#select-localeField");
 inputLocale.addEventListener("focusout", function (event) {
-    SetErrorMessage(event);
+  SetErrorMessage(event);
 });
 inputLocale.addEventListener("change", function (event) {
-    SetErrorMessage(event);
+  SetErrorMessage(event);
 });
 
 function SetErrorMessage(event) {
-    if (!event.target.value) {
-        event.target.classList.add("slds-has-error"); 
-    } else {
-        event.target.classList.remove("slds-has-error");
-    }
+  if (!event.target.value) {
+    event.target.classList.add("slds-has-error");
+  } else {
+    event.target.classList.remove("slds-has-error");
+  }
 
-    ValidateFields(document.querySelectorAll('[required]'));
+  ValidateFields(document.querySelectorAll("[required]"));
 }
 
 function ValidateFields(fields) {
-    let allFieldsFull = true;
-    
-    for (let i = 0; i < fields.length; i++) {
-        if (fields[i].value == '' || fields[i].value == undefined) allFieldsFull = false;
-    }
+  let allFieldsFull = true;
 
-    if (allFieldsFull) { connection.trigger('updateButton', { button: 'next', text: 'done', enabled: true }); }
-    else { connection.trigger('updateButton', { button: 'next', text: 'done', enabled: false }); }
+  for (let i = 0; i < fields.length; i++) {
+    if (fields[i].value == "" || fields[i].value == undefined)
+      allFieldsFull = false;
+  }
+
+  if (allFieldsFull) {
+    connection.trigger("updateButton", {
+      button: "next",
+      text: "done",
+      enabled: true,
+    });
+  } else {
+    connection.trigger("updateButton", {
+      button: "next",
+      text: "done",
+      enabled: false,
+    });
+  }
 }
-
-
 
 /**
  * Gera uma lista de opções dentro de um select.
- * @param {Node} selectInput 
+ * @param {Node} selectInput
  * @param {Array} optionList [{name: 'Opção1', value: 1}, {name: 'Opção2', value: 2}]
  */
 function CreateOptions(selectInput, optionList) {
-    console.log('Gerando opções do input "' + selectInput.id + '"...');
+  console.log('Gerando opções do input "' + selectInput.id + '"...');
 
-    let firstOption = selectInput.querySelector('option');
-    for (let i = 0; i < optionList.length; i++) {
-        let newOption = document.createElement('option');
-        newOption.value = optionList[i].value;
-        firstOption.after(newOption);
-        newOption.innerText = optionList[i].name;
-    }
+  let firstOption = selectInput.querySelector("option");
+  for (let i = 0; i < optionList.length; i++) {
+    let newOption = document.createElement("option");
+    newOption.value = optionList[i].value;
+    firstOption.after(newOption);
+    newOption.innerText = optionList[i].name;
+  }
 }
 
-
-
 /* Execução dos eventos */
-connection.on( "requestedTriggerEventDefinition", (eventDefinitionModel) => { // ? Executado quando a ação "requestTriggerEventDefinition" ocorrer. Atualmente ao abrir a atividade.
-    console.log('Avaliando o Entry Source...');
-    if (eventDefinitionModel) {
-        eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
-    }
+connection.on("requestedTriggerEventDefinition", (eventDefinitionModel) => {
+  // ? Executado quando a ação "requestTriggerEventDefinition" ocorrer. Atualmente ao abrir a atividade.
+  console.log("Avaliando o Entry Source...");
+  if (eventDefinitionModel) {
+    eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
+  }
 });
-connection.on("requestedSchema", (response) => { // ? Executado quando a ação "requestSchema" ocorrer. Atualmente ao abrir a atividade.
-    console.log('Trazendo dados do Entry Source...');
-    for (let item of response.schema) {
-        schema[item.name] = "{{" + item.key + "}}";
-        dataExtensionFields.push({ name: item.name, type: item.type, value: item.name});
-    }
+connection.on("requestedSchema", (response) => {
+  // ? Executado quando a ação "requestSchema" ocorrer. Atualmente ao abrir a atividade.
+  console.log("Trazendo dados do Entry Source...");
+  for (let item of response.schema) {
+    schema[item.name] = "{{" + item.key + "}}";
+    dataExtensionFields.push({
+      name: item.name,
+      type: item.type,
+      value: item.name,
+    });
+  }
 
-    schema.phoneNumber = "{{Contact.Default.PhoneNumber}}";
-    schema.ContactKey = "{{Contact.Default.ContactKey}}";
-    schema.ContactID = "{{Contact.Default.ContactID}}";
+  schema.ContactKey = "{{Contact.Key}}";
+  schema.ContactID = "{{Contact.ID}}";
 
-    CreateOptions(inputLocale, dataExtensionFields.filter((option) => option.type === 'Locale')); // * Foi colocado dessa forma, pois a adição do campo só pode ser feita após a inserção dos dados de "schema"
+  CreateOptions(
+    inputLocale,
+    dataExtensionFields.filter((option) => option.type === "Locale")
+  ); // * Foi colocado dessa forma, pois a adição do campo só pode ser feita após a inserção dos dados de "schema"
 
-    if (createdData.metaData.optStatus) inputOptin.value = createdData.metaData.optStatus; // * Precisa ser executado após a criação das opções nos seletores
-    if (createdData.metaData.localeField) inputLocale.value = createdData.metaData.localeField;
+  if (createdData.metaData.optStatus)
+    inputOptin.value = createdData.metaData.optStatus; // * Precisa ser executado após a criação das opções nos seletores
+  if (createdData.metaData.localeField)
+    inputLocale.value = createdData.metaData.localeField;
 
-    ValidateFields(document.querySelectorAll('[required]'));
+  ValidateFields(document.querySelectorAll("[required]"));
 });
-connection.on("initActivity", (data) => { // * Executado quando a atividade é aberta no Journey Builder
-    console.log('Iniciando a atividade...');
-    createdData = { ...createdData, ...data };
-    body = {...body, ...((createdData.arguments || {execute:{body:'{}'}}).execute.body)};
-    createdData.isConfigured = false;
-    createdData.arguments.execute.inArguments = [];
+connection.on("initActivity", (data) => {
+  // * Executado quando a atividade é aberta no Journey Builder
+  console.log("Iniciando a atividade...");
+  createdData = { ...createdData, ...data };
+  
+  // ESSE BODY NAO EXISTE!!!!!!!!!!!!!!!!!
+  // body = {
+  //   ...body,
+  //   ...(createdData.arguments || { execute: { body: "{}" } }).execute.body,
+  // };
+  createdData.isConfigured = false;
+  createdData.arguments.execute.inArguments = [];
 });
-connection.on("requestedTokens", (tokens) => { // * Executado quando a ação "requestTokens" ocorrer. Atualmente ao abrir a atividade.
-    console.log('Obtendo tokens...');
-    authTokens = tokens;
+connection.on("requestedTokens", (tokens) => {
+  // * Executado quando a ação "requestTokens" ocorrer. Atualmente ao abrir a atividade.
+  console.log("Obtendo tokens...");
+  authTokens = tokens;
 });
-connection.on("clickedNext", () => { // * Executado quando o botão de Next/Done for clicado
-    /* Dados que serão lembrados pela Custom Activity */
-    createdData.metaData.isConfigured = true;
-    createdData.isConfigured = true;
-    createdData.metaData.optStatus = inputOptin.value;
-    createdData.metaData.localeField = inputLocale.value;
+connection.on("clickedNext", () => {
+  // * Executado quando o botão de Next/Done for clicado
+  /* Dados que serão lembrados pela Custom Activity */
+  createdData.metaData.isConfigured = true;
+  createdData.isConfigured = true;
+  createdData.metaData.optStatus = inputOptin.value;
+  createdData.metaData.localeField = inputLocale.value;
 
-    /* Dados enviados quando a ação de execução é chamada */
-    createdData.arguments.execute.inArguments.push(schema);
-    body.optStatus = inputOptin.value;
-    body.localeField = inputLocale.value;
-    createdData.arguments.execute.body = JSON.stringify(body);
+  /* Dados enviados quando a ação de execução é chamada */
+  createdData.arguments.execute.inArguments.push(schema);
+  body.optStatus = inputOptin.value;
+  body.localeField = inputLocale.value;
+  // createdData.arguments.execute.body = body;
 
-    if (schema.undefined) delete schema.undefined;
+  if (schema.undefined) delete schema.undefined;
 
-    console.log(JSON.stringify(createdData));
-    connection.trigger("updateActivity", createdData);
+  console.log(JSON.stringify(createdData));
+  connection.trigger("updateActivity", createdData);
 });
+
