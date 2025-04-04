7fe949f85b0eb607885b32e3bc7b10af784f6bd1
rainer.wuttke@leoo.com.br <rainerzabel1@gmail.com>
2025-04-04 13:57:55 -0300

    Backend Setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = require('./router');
 const path = require('path');
+const axios = require('axios');
 
 const app = express();
 
@@ -8,37 +9,74 @@ app.use(express.json())
 // app.use(express.static('./src'))
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.post('/api/save',(resquest,response) => {
+app.post('/api/save',(req,resp) => {
     console.log('Salvando jornada...');
-    response.status(200).json()
+    resp.status(200).json()
 });
-app.post('/api/validate',(resquest,response) => {
+app.post('/api/validate',(req,resp) => {
     console.log('Validando jornada...');
-    response.status(200).json()
+    resp.status(200).json()
 });
-app.post('/api/publish',(resquest,response) => {
+app.post('/api/publish',(req,resp) => {
     console.log('Publicando jornada...');
-    response.status(200).json()
+    resp.status(200).json()
 });
-app.post('/api/stop',(resquest,response) => {
+app.post('/api/stop',(req,resp) => {
     console.log('Parando jornada...');
-    response.status(200).json()
+    resp.status(200).json()
 });
-app.post('/api/execute',(resquest,response) =>
+app.post('/api/execute', async (req,resp) =>
     {
-        // let {metadata, ...cleanData} = resquest.body;
         console.log('Obtendo dados do Contato...');
-        console.log(resquest.body);
-
-        const contactId = resquest.body.InArguments[0].ContactID;
-        const phone = resquest.body.InArguments[1].usedPhoneField;
-        const locale = resquest.body.InArguments[1].usedLocaleField;
-        const status = resquest.body.InArguments[1].optinStatus;
-
-        
-        return response.status(200).json(); 
-    }
+        try {
+            const contactId = req.body.inArguments[0].ContactID;
+            const phone = req.body.inArguments[1].usedPhoneField;
+            const locale = req.body.inArguments[1].usedLocaleField;
+            const status = req.body.inArguments[1].optinStatus;
+            
+            token = await GetToken();
+    
+            console.log(token); // ! Está retornando um Promise (Tem que arrumar)
+    
+            // Avaliar o status do clinte
+            const userWhatsappStatus = '';
+    
+            if (status == 1) {
+                // OPTIN: Se não tiver optin, deixar como optin
+            } else {
+                // OPTOUT: Se for optin não tiver opção,  deixar como optout
+            }
+    
+            return resp.status(200).json();  
+        } catch (error) {
+            return resp.status(500).json({ error: 'Error while trying to process the request' });
+        }
+    }  
 );
 
 module.exports = app;
+
+async function GetToken() {
+    // ! Pegar o token apenas uma vez e armazenar em cash
+    // Pegar Token API
+    try {
+        const response= await axios.post(
+            'https://mczy4z4pr0jrrrmchxtpzh9kj180.auth.marketingcloudapis.com/v2/token', // Endpoint
+            { // Body
+                grant_type: "client_credentials",
+                client_id: "1q3aa5k724nu7xkbt26vjuhr",
+                client_secret: "tG0cElcwqZsEXMYyPdQ4LWqQ",
+                account_id: "110005209"
+            }
+        )
+    
+        const token = "Bearer " + response.data.access_token;
+        const tokenExpiracion = response.data.expires_in;
+    
+        return token
+    } catch (error) {
+        console.error("Erro ao obter token:", error.response?.data || error.message);
+        return error
+    }
+}
 
